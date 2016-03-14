dbbddff58d96698ae7805d9e33e4d164b16e7173
candr <candr@DESKTOP-H1QC54G>
2016-03-14 18:19:22 -0500

    Signed-off-by: candr <candr@DESKTOP-H1QC54G>

--- ProjectBaseWEB/src/test/java/com/carloscardona/projectbase/web/controller/ProjectBaseTest.java
+++ ProjectBaseWEB/src/test/java/com/carloscardona/projectbase/web/controller/ProjectBaseTest.java
@@ -1,48 +1,55 @@
 package com.carloscardona.projectbase.web.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Locale;
 
+import javax.servlet.ServletContext;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.ui.Model;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/appContext-projectBaseWEB-test.xml" })
-@WebAppConfiguration
 public class ProjectBaseTest {
 
-	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext wac;
 
-	@InjectMocks
-	private WebApplicationContext webApplicationContext;
+	private InicioController inicioController = new InicioController();
+
+	private MockHttpServletRequest request;
+
+	private ServletContext servletContext;
+
+	Model model;
 
 	@Before
-	public void setup() {
-		MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	public void before() throws Exception {
+		this.request = new MockHttpServletRequest();
+		this.servletContext = new MockServletContext();
 	}
 
 	@Test
-	public void testInicio() throws Exception {
-		mockMvc.perform(get("/")).andExpect(status().isOk());
+	public void testInicio() {
+		try {
+			String mav = inicioController.home(new Locale("es"), model);
+			Assert.assertEquals("inicio", mav);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testMenu() {
+		String mav = "";
 		try {
-			mockMvc.perform(post("/addNewApplicant").param("key1", "value1").param("key2", "value2")).andExpect(status().isOk());
+			mav = inicioController.onSubmit(request, model);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Assert.assertEquals("menu", mav);
 	}
 }
